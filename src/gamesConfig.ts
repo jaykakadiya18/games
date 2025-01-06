040b84fd831e082d5dd9a8266684d2e8e5dd3039
jaykakadiya18 <jaykakadiya2014@gmail.com>
2025-01-06 13:29:17 +0530

    fixed typo error

--- src/gamesConfig.ts
+++ src/gamesConfig.ts
@@ -6,5 +6,6 @@ import { Game } from './components/Snake/Game';
 export const games = [
   { id: 1, name: 'Flip & Find', component: GameBoard },
   { id: 2, name: 'Snake', component: Game },
+  
   // Add more games here
 ];
